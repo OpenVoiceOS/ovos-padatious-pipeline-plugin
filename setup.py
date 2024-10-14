b8f69063e1abba0e0649b0205e1e5645e5234c55
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-10-14 03:15:06 +0100

    feat:semver (#3)

--- setup.py
+++ setup.py
@@ -1,19 +1,57 @@
 #!/usr/bin/env python3
-from os.path import join, abspath, dirname
+
+import os
+import os.path
+
 from setuptools import setup
 
-with open(join(dirname(abspath(__file__)), 'requirements.txt')) as f:
-    requirements = f.readlines()
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+def get_version():
+    """ Find the version"""
+    version_file = os.path.join(BASEDIR, 'ovos_padatious', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if int(alpha):
+        version += f"a{alpha}"
+    return version
+
+
+with open(os.path.join(BASEDIR, "README.md"), "r") as f:
+    long_description = f.read()
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
 
 PLUGIN_ENTRY_POINT = 'ovos-padatious-pipeline-plugin=ovos_padatious.opm:PadatiousPipeline'
 
-
 setup(
     name='ovos-padatious',
-    version='0.4.8',  # Also change in ovos_padatious/__init__.py
+    version=get_version(),
     description='A neural network intent parser',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -23,7 +61,7 @@ setup(
     packages=[
         'ovos_padatious'
     ],
-    install_requires=requirements,
+    install_requires=required('requirements.txt'),
     zip_safe=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
